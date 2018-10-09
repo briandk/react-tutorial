3d5aad191834862233fb6d3296231a39bff31f66
Brian Danielak <briandaniela.k+github@gmail.com>
2018-10-09 12:42:33 -0400

    Render game instead of boilerplate
    from create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { Game } from "./components/Game";
 
 import "./App.css";
-import logo from "./logo.svg";
 
-class App extends React.Component {
+export class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Game />
       </div>
     );
   }
 }
-
-export default App;
-
-///////// React tutorial starter code
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
